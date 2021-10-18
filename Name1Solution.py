5e7545e4d8c699c8c1a9fbb1be4b52a298bdb090
Micah <micnorman@paypal.com>
2021-10-18 16:49:32 -0500

    solution 1 single function

--- Name1Solution.py
+++ Name1Solution.py
@@ -1,9 +1,9 @@
 # Assignment criteria
-# For the first name: Ignore the first letter, then find the first consonant and 
+# For the first name: Ignore the first letter, then find the first consonant and
 # keep everything before and including the first consonant.
 
-# For the second name: If it starts with a vowel, keep the whole second name. 
-# If it starts with a consonant, keep every letter after the first consonant, but not including it. 
+# For the second name: If it starts with a vowel, keep the whole second name.
+# If it starts with a consonant, keep every letter after the first consonant, but not including it.
 
 # Set Constants
 CONSONANT: str = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
@@ -11,58 +11,49 @@ VOWEL: str = "aeiouAEIOU"
 DEBUG: bool = False
 
 # Set Unit Test
-## I am not sure if you have done dicts yet, but this would usually be a list of dicts in the form of the following.
+# I am not sure if you have done dicts yet, but this would usually be a list of dicts in the form of the following.
 unit_tests: list[dict] = [
     {
         "testname": "First Name process",
         "test_content": "Korra",
         "expected_return": "K",
         "expect_error": False
-     }    
+    }
 ]
 
-## Im going to proceed with this method though.  Just a list of the test content.
+# Im going to proceed with this method though.  Just a list of the test content.
+# Did you get instrunction on what to print in case of an edge case.  I am assuming just print a blank line which is what i have done.
 first_name_test_cases: list[str] = [
-    "Korra",
-    "K", # edge case, only 1 char
-    "Koaeeoau", # edge case no subsequent consonants
-    "", # edge case empty name
-    "ooaaeeiio" # edge case: no consonants
+    "Korra",  # Expect: Kor,
+    "Z",  # Expect: '', edge case, only 1 char.
+    "Poaeeoau",  # Expect: '', edge case no subsequent consonants
+    "",  # Expect: '', edge case empty name
+    "ooaaeeiio",  # Expect: '', edge case: no consonants
+    "JP",  # Expect: JP, No Vowel
+    "Aaron"  # Expect: 'Aar', normal case
 
 ]
 
 '''
-    parse_first_name takes a string, drops the first letter.  It then scans to the point of the first consonant and using that index number returns the input string from 0 to that index number + 1
+    matching_to_first_consonent takes a string, drops the first letter.  It then scans to the point of the first consonant and using that index number returns the input string from 0 to that index number + 1
 '''
-# Since i am not doing exception handling i am breaking this up into 2 functions.
-## With exception handling, this can all be done on a single line of code but would be much less readable.
-def parse_first_name(n: str)-> str:
-    # Accept n=Korra.
-    # Drop first letter. n[1:]
-    # For n[n_idx+1]
-        # if n[n_idx+1] in CONSONANT
-        # return n_consonant_index
-    # return n[0:(n_consonant_index+1)]
-    print(get_index_of_first_consonant(n)+1)
-    return f"{n[0]}{n[1:(get_index_of_first_consonant(n)+2)]}" # returns the `first letter in variable n` + all letters from the second letter in n up to and including the first consonant.
 
-def get_index_of_first_consonant(n: str) -> int:
+
+def matching_to_first_consonent(n: str) -> str:
     # Would usually do this with exception handling, but you might not be there yet.
-    err_val: int = -1 # if err_val is returned, something unexpected has happened.
-    for c in enumerate(n[1:]):
-        if c[1] in CONSONANT:
-            return c[0] 
+    err_val: str = ""  # if err_val is returned, something unexpected has happened.
+    for c in enumerate(n):
+        if c[1] in CONSONANT and c[0] != 0:
+            return n[0:c[0]+1]
     return err_val
 
-n = parse_first_name(first_name_test_cases[0])
-print(n)
 
-
-
+# This is how you can run all of your test cases.
+for test in first_name_test_cases:
+    print(matching_to_first_consonent(test))
 
 
 # second_name:str = "Asami"
-
 
 
 # def parse_second_name(n: str)-> str:
